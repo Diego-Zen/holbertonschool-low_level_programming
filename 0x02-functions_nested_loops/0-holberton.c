c75905666c53126f5b81c8d170fec5f886eea8d0
Diego Monroy <difemo91@gmail.com>
2019-06-24 21:23:02 +0000

    CREATE putchar c program to print holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+/**
+ * main - entry point
+ *
+ * Print holberton
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _putchar(char c);
+
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
