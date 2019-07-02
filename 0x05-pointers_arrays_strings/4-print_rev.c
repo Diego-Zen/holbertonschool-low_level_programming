c99bd0ce225e80e657df1f8732519fcc582fe5cf
Diego Monroy <difemo91@gmail.com>
2019-07-02 16:26:04 +0000

    Add reverse string c program

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: Pointer char
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		if (s[i] == '\0')
+		{
+			j = i;
+			while (j >= 0)
+			{
+				_putchar(s[j]);
+				j--;
+			}
+		}
+	}
+	_putchar('\n');
+}
