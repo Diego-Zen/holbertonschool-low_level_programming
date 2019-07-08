5a9d47b58914cd465df74d652cd6f0ccb7714fcc
Diego Monroy <difemo91@gmail.com>
2019-07-08 20:57:49 +0000

    Add print chessboard c program

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * *_strpbrk - Search a string for any of a set of bytes
+ *
+ * @s: char string
+ * @accept: char string
+ *
+ * Return: char pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	char *loc;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				loc = &s[i];
+				break;
+			}
+		}
+	}
+	return (loc);
+}
