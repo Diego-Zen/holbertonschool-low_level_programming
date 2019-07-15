30ee1d4f35fe09ae0ff1b75719001149a5436d9e
Diego Monroy <difemo91@gmail.com>
2019-07-15 01:18:33 +0000

    Add number of arguments c program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Prints the number of arguments
+ *
+ * @argc: integer count arguments
+ * @argv: string vector arguments
+ *
+ * Return: Success
+ */
+int main(int argc, char __attribute__((__unused__))*argv[])
+{
+       	printf("%d\n", argc - 1);
+
+	return (0);
+}
