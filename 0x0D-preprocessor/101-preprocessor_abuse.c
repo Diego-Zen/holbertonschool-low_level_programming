2ad29411b4f2aad5b3df57985d23cb9abe698426
Diego Monroy <difemo91@gmail.com>
2019-07-22 20:50:18 +0000

    Remove additional lines

--- 0x0D-preprocessor/101-preprocessor_abuse.c
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-#define TEXT "Hello, Holberton"
-/**
- * main - print.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("%s\n", TEXT);
-	return (0);
-}
+#define PRINT(s) printf(#s"\n")
