397b58499a730fb0a8815f96f585d22ddca2a020
Diego Monroy <difemo91@gmail.com>
2019-07-23 04:52:33 +0000

    Add free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,12 +9,7 @@
  * @owner: char owner
  *
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void free_dog(dog_t *d)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+        free(d);
 }
