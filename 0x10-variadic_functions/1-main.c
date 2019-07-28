524fe24be923dd7a8b50b59ae22011978c239149
Diego Monroy <difemo91@gmail.com>
2019-07-28 15:39:57 +0000

    Add validation for separator

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -10,5 +10,8 @@ int main(void)
 	print_numbers(", ", 4, 0, 98, -1024, 402);
 	print_numbers("; ", 2, 0, 100);
 	print_numbers("- ", 0);
+	print_numbers(", ", 1, 100);
+	print_numbers("", 1, 10);
+	print_numbers(NULL, 2, 4, 10);
 	return (0);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,7 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	if (separator != NULL && n != 0)
+	if (n != 0)
 	{
 		va_list args;
 		unsigned int i;
@@ -17,7 +17,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(args, n);
 		for (i = 0; i < n - 1; i++)
 		{
-			printf("%d%s", va_arg(args, int), separator);
+			printf("%d", va_arg(args, int));
+			if (separator != NULL)
+				printf("%s", separator);
 		}
 		printf("%d", va_arg(args, int));
 		va_end(args);
