689a3151adf247f870d4355ae3f03bf39a292fa0
Diego Monroy <difemo91@gmail.com>
2019-07-29 02:12:32 +0000

    Add print string in assembly

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,15 @@
+section .data				;data section for declaring constants
+	msg db "Hello, Holberton"	;msg constant
+
+section .text				;text section is for code
+	global _start			;tells the kernel where to start
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 16
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
