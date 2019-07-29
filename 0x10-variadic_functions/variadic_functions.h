e68be0594749d8eaf78cef8be2d47b9642de52fe
Diego Monroy <difemo91@gmail.com>
2019-07-29 00:26:56 +0000

    Add function for print everything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,25 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdlib.h>
 #include <stdarg.h>
+/**
+ * struct optype
+ *
+ * @c: char types
+ * @f: pointer to function
+ *
+ */
+typedef struct optype
+{
+	char *c;
+	void (*f)();
+}optype_t;
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list);
+void print_integer(va_list);
+void print_float(va_list);
+void print_string(va_list);
 #endif
