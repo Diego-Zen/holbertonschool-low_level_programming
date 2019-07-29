b7cc460ce42ba04fa46cc71dcdf6f89baa4f17c5
Diego Monroy <difemo91@gmail.com>
2019-07-29 02:38:30 +0000

    Fix print new line

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,8 +1,9 @@
 section .data				;data section for declaring constants
-	msg db "Hello, Holberton"	;msg constant
+	msg db "Hello, Holberton",0x0a,0	;msg constant
 
 section .text				;text section is for code
 	global _start			;tells the kernel where to start
+
 _start:
 	mov rax, 1
 	mov rdi, 1
