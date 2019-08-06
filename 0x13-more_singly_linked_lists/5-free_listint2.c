47995921c89e386b732f31f5aca7ede3346373cd
Diego Monroy <difemo91@gmail.com>
2019-08-06 20:02:55 +0000

    Add free2 function

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * free_listint2 -
+ * free_listint2 - frees a list
  *
- * Return: 
+ * @head: pointer list
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *current;
 
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = current->next;
+		free(current);
+	}
+	*head = NULL;
 }
