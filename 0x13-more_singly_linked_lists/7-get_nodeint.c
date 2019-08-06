286ca31a97604e4b8910ea5c65bffc0aa1b7bb79
Diego Monroy <difemo91@gmail.com>
2019-08-06 21:30:27 +0000

    add index of a node function

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index -
+ * get_nodeint_at_index - find the nth node of a linked list
  *
- * Return: 
+ * @head: pointer to list
+ * @index: node
+ *
+ * Return: the index node or NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+	listint_t *current = head;
 
+	while (current != NULL)
+	{
+		if (i == index)
+			return (current);
+		current = current->next;
+		i++;
+	}
+	return (NULL);
 }
