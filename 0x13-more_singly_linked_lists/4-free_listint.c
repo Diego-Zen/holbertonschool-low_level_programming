ab567c0bda8dbf0abdb3ceea03afc26926afa225
Diego Monroy <difemo91@gmail.com>
2019-08-06 17:26:28 +0000

    add free function

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * free_listint -
+ * free_listint - frees a list
  *
- * Return: 
+ * @head: list pointer
  */
 void free_listint(listint_t *head)
 {
+	listint_t *current;
 
+	while (head != NULL)
+	{
+		current = head;
+		head = current->next;
+		free(current);
+	}
 }
