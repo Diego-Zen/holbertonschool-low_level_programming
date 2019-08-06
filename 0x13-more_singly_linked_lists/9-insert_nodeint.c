a9ac8927eb228ce0114f4cdd2e84b415c21c47da
Diego Monroy <difemo91@gmail.com>
2019-08-06 23:19:33 +0000

    add validation when index is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,8 +24,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = NULL;
-
+	if (idx == 0)
+	{
+		new_node->next = current;
+		*head = new_node;
+		return (new_node);
+	}
 	while (current != NULL)
 	{
 		if (i == idx)
