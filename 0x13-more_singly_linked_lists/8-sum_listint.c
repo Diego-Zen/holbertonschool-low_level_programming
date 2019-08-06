c62ad4c921f86efb5e52bda671f66de0cb683104
Diego Monroy <difemo91@gmail.com>
2019-08-06 22:18:11 +0000

    add sum values of list function

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * sum_listint -
+ * sum_listint - sums all the data n of a list
  *
- * Return: 
+ * @head: pointer to list
+ *
+ * Return: sum of all the data
  */
 int sum_listint(listint_t *head)
 {
+	listint_t *current;
+	int sum = 0;
 
+	if (head == NULL)
+		return (0);
+
+	current = head;
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
 }
