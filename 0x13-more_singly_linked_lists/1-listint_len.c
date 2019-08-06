371b11d7b7475af388e810e5c654d7353a799500
Diego Monroy <difemo91@gmail.com>
2019-08-06 03:29:06 +0000

    add function for adding nodes

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * listint_len -
+ * listint_len - number of elements in a list
  *
- * Return: 
+ * @h: list
+ *
+ * Return: the number of elements of a list
  */
 size_t listint_len(const listint_t *h)
 {
+	int i;
 
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+
+	return (i);
 }
