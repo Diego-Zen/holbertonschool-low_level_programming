932f277843bc71568f064019fbb769c3a818b0fc
Diego Monroy <difemo91@gmail.com>
2019-08-08 23:20:14 +0000

    add binary representation of number program

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * print_binary -
+ * print_binary - prints the binary representation of a number
  *
- * Return: 
+ * @n: number
  */
 void print_binary(unsigned long int n)
 {
-
+	if (n > 0)
+	{
+		print_binary(n >> 1);
+		printf("%lu", n & 1);
+	}
 }
