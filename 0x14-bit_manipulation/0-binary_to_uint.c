9b28dbf93b279ab1013a5e93ed41a25109b79706
Diego Monroy <difemo91@gmail.com>
2019-08-08 21:05:44 +0000

    add binary to integer function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint -
+ * binary_to_uint - converts a binary to an unsigned int
  *
- * Return: 
+ * @b: string
+ *
+ * Return: the converted number or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int sum;
 
+	if (b == NULL)
+		return (0);
+
+	for (sum = 0; *b; b++)
+	{
+		if (*b == '1')
+			sum = (sum << 1) | 1;
+		else if (*b == '0')
+			sum = sum << 1;
+		else
+			return (0);
+	}
+
+	return (sum);
 }
