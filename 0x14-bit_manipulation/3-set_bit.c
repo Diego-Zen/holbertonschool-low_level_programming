346dc005fac96c90949d22d2d3c8bec3eb147833
Diego Monroy <difemo91@gmail.com>
2019-08-09 04:45:49 +0000

    add validation for index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,8 +12,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int num = 1;
 
-	if (index > sizeof(unsigned int) * 8)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
+
 	num = num << index;
 	*n = *n | num;
 	return (1);
