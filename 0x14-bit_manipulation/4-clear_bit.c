eaed886178554b55ed39c7967d3eb80f3c68e178
Diego Monroy <difemo91@gmail.com>
2019-08-09 04:51:13 +0000

    add clear bit program

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
+ * clear_bit - sets the value of a bit to 0
  *
- * Return: 
+ * @n: pointer to number
+ * @index: position
+ *
+ * Return: 1 or -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int num = 1;
 
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+
+	num = num << index;
+	*n = *n & ~num;
+	return (1);
 }
