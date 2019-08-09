5aca53565d4c520478f0004225d094d353e59396
Diego Monroy <difemo91@gmail.com>
2019-08-09 04:42:28 +0000

    add set value of a bit program

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * set_bit -
+ * set_bit - sets the value of a bit to 1 at a given index
  *
- * Return: 
+ * @n: number
+ * @index: position
+ *
+ * Return: 1 or -1
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int num = 1;
 
+	if (index > sizeof(unsigned int) * 8)
+		return (-1);
+	num = num << index;
+	*n = *n | num;
+	return (1);
 }
