adb2afe072503d102549cb608cefb37753133dd1
Diego Monroy <difemo91@gmail.com>
2019-08-09 04:27:47 +0000

    add get git at index program

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,19 @@
 #include "holberton.h"
 
 /**
- * get_bit -
+ * get_bit - get value of a bit at a given index
  *
- * Return: 
+ * @n: number
+ * @index: position
+ *
+ * Return: the value of the bit at index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	if ((n >> index) & 1)
+		return (1);
+	else
+		return (0);
 }
