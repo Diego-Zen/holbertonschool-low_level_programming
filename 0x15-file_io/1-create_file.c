6eeb9f8d1b295d8196e0fd3febb6bf574438ea45
Diego Monroy <difemo91@gmail.com>
2019-08-20 04:12:21 +0000

    add return error type

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
+
 	copy = text_content;
 	if (text_content != NULL)
 	{
