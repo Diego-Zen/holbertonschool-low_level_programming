7c20e7ee992097a433f2c913b352aa61a44357b7
Diego Monroy <difemo91@gmail.com>
2019-09-16 04:57:02 +0000

    add sum nodes

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
+ * sum_dlistint - sum of all the data
  *
- * Return: 
+ * @head: pointer head list
+ *
+ * Return: return the sum of all the data or 0 if it is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *tmp;
+	int sum = 0;
 
+	tmp = head;
+
+	if (tmp == NULL)
+		return (0);
+	while (tmp != NULL)
+	{
+		sum = sum + tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
 }
