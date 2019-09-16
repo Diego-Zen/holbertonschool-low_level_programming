e776bfff2199e36ef6101a5231312ce2a593214f
Diego Monroy <difemo91@gmail.com>
2019-09-16 05:00:17 +0000

    add bety style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,9 +1,13 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index -
+ * insert_dnodeint_at_index - inserts a new node at the given position
  *
- * Return: 
+ * @h: pointer head
+ * @idx: index
+ * @n: integer
+ *
+ * Return: the address of the new node or NULL if it fails
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,9 +1,12 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index -
+ * delete_dnodeint_at_index - deletes node at index
  *
- * Return: 
+ * @head: pointer head
+ * @index: index
+ *
+ * Return: 1 if success or -1 if it fails
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
