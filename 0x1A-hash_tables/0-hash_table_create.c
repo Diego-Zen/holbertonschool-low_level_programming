7b048b877e7eb7b6b7a12a7f457efe4b39fd754c
Diego Monroy <difemo91@gmail.com>
2019-10-12 04:10:05 +0000

    add initialization of every variable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ht = NULL;
+	unsigned long int i = 0;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
@@ -16,6 +17,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t) * size);
 	if (ht->array == NULL)
 		return (NULL);
+
+	ht->size = size;
+	while (i < size)
+	{
+		ht->array[i] = NULL;
+		i++;
+	}
 
 	return (ht);
 }
