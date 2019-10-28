54c43424c9b8835ed35bdba839f822ff06dade18
Diego Monroy <difemo91@gmail.com>
2019-10-28 23:56:44 +0000

    add bubble sort algorithm

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -9,5 +9,23 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	size_t tmp = 0;
 
+	if (size > 0 && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
+					print_array(array, size);
+				}
+			}
+		}
+	}
 }
