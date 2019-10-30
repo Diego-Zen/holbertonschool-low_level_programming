25d50645a0122a61713e7e57a9d00a8dad1d5e71
Diego Monroy <difemo91@gmail.com>
2019-10-30 02:48:50 +0000

    add selection sort

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -8,5 +8,25 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i;
+	size_t j;
+	size_t min = 0;
+	int swap;
 
+	if (size > 0 && array != NULL)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			min = i;
+			for (j = i + 1; j < size; j++)
+			{
+				if (array[min] > array[j])
+					min = j;
+			}
+			swap = array[min];
+			array[min] = array[i];
+			array[i] = swap;
+			print_array(array, size);
+		}
+	}
 }
