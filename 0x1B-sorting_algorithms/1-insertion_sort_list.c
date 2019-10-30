6ddf536c763d55bc60bbdeaac4a7289ef9ae0a1d
Diego Monroy <difemo91@gmail.com>
2019-10-30 00:39:02 +0000

    add insertion sort

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -7,5 +7,49 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *prevnode = NULL;
+	listint_t *nxnode = NULL;
+	listint_t *current = NULL;
 
+
+	if (list != NULL && (*list)->next != NULL)
+	{
+		current = (*list)->next;
+		prevnode = current->prev;
+		nxnode = current->next;
+
+		while (current != NULL)
+		{
+			while (prevnode != NULL && prevnode->n > current->n)
+			{
+				current->next = prevnode;
+				current->prev = prevnode->prev;
+
+				if (prevnode->prev != NULL)
+					(prevnode->prev)->next = current;
+
+				prevnode->prev = current;
+				prevnode->next = nxnode;
+
+				if (nxnode != NULL)
+					nxnode->prev = prevnode;
+
+				nxnode = prevnode;
+				prevnode = current->prev;
+
+				if ((*list)->prev != NULL)
+					*list = (*list)->prev;
+
+				print_list(*list);
+			}
+			current = nxnode;
+
+			if (current != NULL)
+			{
+				nxnode = current->next;
+				prevnode = current->prev;
+			}
+
+		}
+	}
 }
