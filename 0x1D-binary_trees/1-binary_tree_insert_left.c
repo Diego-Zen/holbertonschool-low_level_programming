e460891006678279b9e3dcb6a987d4c4366b3dfa
Diego Monroy <difemo91@gmail.com>
2019-12-04 23:35:02 +0000

    sech part added

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -8,26 +8,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_left_node = binary_tree_node(parent, value);
 
-	new_node = malloc(sizeof(*new_node));
-	if (!new_node || !parent)
+	if (!parent)
 		return (NULL);
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
 
 	if (parent->left)
 	{
-		(parent->left)->parent = new_node;
-		new_node->left = parent->left;
-		parent->left = new_node;
+		new_left_node->left = parent->left;
+		(parent->left)->parent = new_left_node;
+		parent->left = new_left_node;
+		return (new_left_node);
 	}
 	else
 	{
-		parent->left = new_node;
+		parent->left = new_left_node;
+		return (new_left_node);
 	}
-	return (new_node);
 }
