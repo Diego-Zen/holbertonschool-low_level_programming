7754074c4438bdafbf7efeae6be7fae5ef012003
Diego Monroy <difemo91@gmail.com>
2019-12-07 00:04:47 +0000

    sech part fixed

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -8,10 +8,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left_node = binary_tree_node(parent, value);
+	binary_tree_t *new_left_node = NULL;
 
 	if (!parent)
 		return (NULL);
+
+	new_left_node = binary_tree_node(parent, value);
 
 	if (parent->left)
 	{
